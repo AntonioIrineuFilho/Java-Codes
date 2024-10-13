7625ac6368838118a9c6c5c5e2592b075526dd2f
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-10-13 02:50:01 +0000

    create

--- Peso.java
+++ Peso.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Peso {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o peso total das cargas: ");
+        int peso = Integer.parseInt(scanner.nextLine());
+        System.out.print("Digite a capacidade de cada container: ");
+        int capacidade = Integer.parseInt(scanner.nextLine());
+        int containers = peso / capacidade;
+        if (peso % capacidade != 0) {
+            containers = peso / capacidade + 1;
+        }
+        System.out.println(String.format("Quantidade de containers: %d\nPeso por container: %d", containers, (peso / containers)));
     }
 }
